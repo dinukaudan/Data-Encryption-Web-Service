915e4876615d91b159cee915694c03d1cc1aef07
dinukaudan <dinuka.udan@gmail.com>
2019-01-12 01:14:51 +0530

    first commit

---
+++ src/main/java/com/udan/dataencryptionwebservice/DataEncryptionWebServiceApplication.java
@@ -0,0 +1,14 @@
+package com.udan.dataencryptionwebservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataEncryptionWebServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataEncryptionWebServiceApplication.class, args);
+	}
+
+}
+
